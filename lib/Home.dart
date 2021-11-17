85457fada139017a7efff1246214eeec84654473
Marcus Rocha <sif.marcusvinicius@gmail.com>
2021-11-17 16:00:09 -0300

    Implementando o FloatingBUtton

--- lib/Home.dart
+++ lib/Home.dart
@@ -8,6 +8,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List<String> tarefas = ["Teste 1", "Teste 2", "Teste 3"];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +16,50 @@ class _HomeState extends State<Home> {
         title: Text("Lista de Tarefas"),
         backgroundColor: Colors.purple,
       ),
-      body: Text("TAREFAS"),
+      body: Column(
+          //padding: EdgeInsets.all(15),
+          children: [
+            Expanded(
+              child: ListView.builder(
+                  itemCount: tarefas.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(tarefas[index]),
+                    );
+                  }),
+            )
+          ]),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) {
+                return AlertDialog(
+                  title: Text("Adicionar Tarefa"),
+                  content: TextField(
+                    decoration:
+                        InputDecoration(labelText: "Digite sua tarefa:"),
+                    onChanged: (text) {},
+                  ),
+                  actions: [
+                    FlatButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text(
+                          "Cancelar",
+                          style: TextStyle(color: Colors.blue),
+                        )),
+                    FlatButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          "Salvar",
+                          style: TextStyle(color: Colors.blue),
+                        )),
+                  ],
+                );
+              });
           print("Floating button");
         },
         backgroundColor: Colors.purple,
